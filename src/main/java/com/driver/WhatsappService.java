279e8b3ae7c6c4c0e00113227115e86e380bc1dd
Arundhati Mohanty <arundhati.mohanty1@gmail.com>
2023-02-28 15:38:02 +0530

    find message, between specified dates

--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -1,24 +1,26 @@
 package com.driver;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class WhatsappService {
     WhatsappRepository whatsappRepository = new WhatsappRepository();
+
     public String createUser(String name, String mobile) throws Exception {
         return whatsappRepository.createUser(name, mobile);
     }
 
-    public Group createGroup(List<User> users){
+    public Group createGroup(List<User> users) {
         return whatsappRepository.createGroup(users);
     }
 
-    public int createMessage(String content){
+    public int createMessage(String content) {
         return whatsappRepository.createMessage(content);
     }
 
